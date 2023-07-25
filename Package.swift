820aa7d91aa559f298ef5a9769d95469dc3938ca
LimJiseong <yim2627@gmail.com>
2023-07-25 10:53:50 +0900

    proj init

---
+++ Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.7
+
+import PackageDescription
+
+let package = Package(
+    name: "AdBrixRmKit",
+    products: [
+        .library(
+            name: "AdBrixRmKit",
+            targets: ["AdBrixRmKit"]),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,3 @@
+# AdBrixRmKit
+
+A description of this package.
