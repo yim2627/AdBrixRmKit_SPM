7c744c4e94e9b79d05e1d54fd3cf77812d445a1f
LimJiseong <yim2627@gmail.com>
2023-11-02 16:57:53 +0900

    bump v2.5.0.0

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
 	targets: [
 		.binaryTarget(
 			name: "AdBrixRmKit",
-			url: "https://github.com/yim2627/AdBrixRmKit_SPM/releases/download/v2.4.0.6/AdBrixRmKit.xcframework.zip",
-			checksum: "de196dfe07de10c0fbab672f1e3cf4a6cbb66ee3f9a4bcbc0fe2b0b911fce391")
+			url: "https://github.com/yim2627/AdBrixRmKit_SPM/releases/download/v2.5.0.0/AdBrixRmKit.xcframework.zip",
+			checksum: "2ec10367f4af79c6cc4319d0c7a59ee21c1dcd719f028c0235459201d0b937ec")
 	]
 )
